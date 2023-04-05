83cb23b1d5da07f0fba080461199f83aafbac970
Ba5hir <bashir15-12342@diu.edu.bd>
2023-04-05 14:21:13 +0600

    user profile added

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../assets/pngegg.png";
 
 const Header = () => {
   return (
     <nav class="navbar bg-body-tertiary">
       <div class="container-fluid">
         <a class="navbar-brand">AiUniverse</a>
+        <img src={logo} alt="" />
       </div>
     </nav>
   );
