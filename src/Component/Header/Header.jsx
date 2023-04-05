5511fbe85d7264f4c9ad2d35ab5ec29537d9ad8b
Ba5hir <bashir15-12342@diu.edu.bd>
2023-04-05 14:19:40 +0600

    header jsx added

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand">AiUniverse</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
